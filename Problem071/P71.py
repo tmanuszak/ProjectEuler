41204e749616bdb7501cc62ff313ac739fb3fed7
Trey Manuszak <trey17manuszak@yahoo.com>
2020-05-19 11:48:51 -0700

    Added Problem071

---
+++ Problem071/P71.py
@@ -0,0 +1,5 @@
+print(3*142856 + 2)
+# 2/5 is a tangent ford circle to the left of 3/7
+# Solve the equation 7x + 5 = 1,000,000 and you find you need to take 142856 mediants
+# for the denominator to be less than or equal to 1,000,000
+# So, the numerator must be 3*142856 + 2 after that many mediants
